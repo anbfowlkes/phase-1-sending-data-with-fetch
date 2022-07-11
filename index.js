14c5926d734acd4e95edfa4af287c343f209d0da
Aaron Fowlkes <afowlkes21@gmail.com>
2022-07-11 13:24:47 -0400

    commit tree 7a9877de3a5761a76a26bd7bb323b5bd6d02972a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+let submitData = (name, email) => {
+    let dataObj = {
+        name: name,
+        email: email
+    }
+
+    let configurationObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(dataObj)
+        }
+    
+
+    return fetch('http://localhost:3000/users', configurationObj)
+    .then((res) => {
+        return res.json()
+    })
+    .then((obj) => {
+        console.log(obj)
+        let id = obj.id
+        console.log(id)
+        let h3 = document.createElement('h3')
+        h3.innerText = id
+        idDiv = document.getElementById('ids')
+        idDiv.append(h3)
+    })
+    .catch((error) => {
+        let h3 = document.createElement('h3')
+        h3.innerText = error.message
+        errDiv = document.getElementById('errs')
+        errDiv.append(h3)
+    })
+}
+
+
